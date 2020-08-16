69f5ee6ee025ccfbf752f38abd1f891408f32942
Youngju Na <apercent1@naver.com>
2020-08-16 14:52:52 +0900

    Add square function

--- calculator.py
+++ calculator.py
@@ -5,10 +5,11 @@ def add(a, b):
 def subtract(a, b):
 	return a - b
 
-<<<<<<< HEAD
 def divide_free(a, b):
 	return a / b
-=======
+
 def multiply(a, b):
 	return a * b
->>>>>>> 234ed9dd765c8093f24f401ee8dd596d9f6cd5d7
+
+def square(a):
+	return a*a
