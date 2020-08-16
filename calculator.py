3f6919ebbf359c8f7d82a27a4ee6ffdda9ef3fbb
Youngju Na <apercent1@naver.com>
2020-08-16 14:55:32 +0900

    Revert "Add square function"
    This reverts commit 69f5ee6ee025ccfbf752f38abd1f891408f32942.

--- calculator.py
+++ calculator.py
@@ -5,11 +5,10 @@ def add(a, b):
 def subtract(a, b):
 	return a - b
 
+<<<<<<< HEAD
 def divide_free(a, b):
 	return a / b
-
+=======
 def multiply(a, b):
 	return a * b
-
-def square(a):
-	return a*a
+>>>>>>> 234ed9dd765c8093f24f401ee8dd596d9f6cd5d7
